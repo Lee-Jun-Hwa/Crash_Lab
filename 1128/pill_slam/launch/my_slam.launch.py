03d8bc045826f0e5fbe04b85a90d39b65d14eb83
Lee-Jun-Hwa <ik558999@hanyang.ac.kr>
2024-11-28 21:13:00 +0900

    Add files via upload

---
+++ 1128/pill_slam/launch/my_slam.launch.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+
+def generate_launch_description():
+	use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+	cartographer_prefix = get_package_share_directory('cartographer')
+	tutorial_ros2_motor_prefix = get_package_share_directory('tutorial_ros2_motor')
+	pill_description_prefix = get_package_share_directory('pill_description')
+	ydlidar_prefix = get_package_share_directory('ydlidar_ros2_driver')
+
+
+
+	resolution = LaunchConfiguration('resolution', default='0.05')
+	publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
+
+
+
+
+
+	
+	return LaunchDescription([
+		IncludeLaunchDescription(
+			PythonLaunchDescriptionSource([cartographer_prefix, '/launch','/cartographer.launch.py']),
+			launch_arguments={'use_rviz': 'true'}.items()
+		),
+		IncludeLaunchDescription(
+			PythonLaunchDescriptionSource([pill_description_prefix,'/launch','/pill_description.launch.py']),
+			launch_arguments={'use_rviz': 'false'}.items()
+		),
+		IncludeLaunchDescription(
+			PythonLaunchDescriptionSource([ydlidar_prefix, '/launch','/ydlidar_launch.py']),
+    	),
+		Node(
+			package='tutorial_ros2_motor',
+			executable='test_motor_node',
+			name='test_motor_node',
+		)
+	])
